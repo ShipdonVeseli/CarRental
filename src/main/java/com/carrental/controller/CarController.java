745ac47d027098f433862f057c8658b1198eade4
shipdonveseli <veseli.shipdon@gmail.com>
2022-03-30 19:35:51 +0200

    Request mapping for car controller

--- src/main/java/com/carrental/controller/CarController.java
+++ src/main/java/com/carrental/controller/CarController.java
@@ -1,9 +1,11 @@
 package com.carrental.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/cars")
 public class CarController {
     @GetMapping("/car")
     public String index() {
