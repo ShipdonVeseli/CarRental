1da5233d6753dd80ddada574a6d45974d1bdc42e
suekrueyavuz <93677903+suekrueyavuz@users.noreply.github.com>
2022-03-31 11:42:26 +0200

    carservice getallcars

--- src/main/java/com/carrental/service/CarService.java
+++ src/main/java/com/carrental/service/CarService.java
@@ -1,7 +1,26 @@
 package com.carrental.service;
 
+import com.carrental.entity.Car;
+import com.carrental.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CarService {
+    private CarRepository carRepository;
+
+    @Autowired
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    public Car createNewCar(Car car) {
+        return carRepository.save(car);
+    }
+
+    public List<Car> getAllCars() {
+        return carRepository.findAll();
+    }
 }
