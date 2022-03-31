b1ea78e0f8b77a7279b8d5d649e277b148c1276c
suekrueyavuz <93677903+suekrueyavuz@users.noreply.github.com>
2022-03-31 11:44:06 +0200

    api getallcars

--- src/main/java/com/carrental/controller/CarController.java
+++ src/main/java/com/carrental/controller/CarController.java
@@ -1,14 +1,31 @@
 package com.carrental.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.carrental.entity.Car;
+import com.carrental.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cars")
 public class CarController {
-    @GetMapping("/car")
-    public String index() {
-        return "Get all cars";
+    private CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @PostMapping
+    public Car createNewCar(@RequestBody Car car) {
+        return carService.createNewCar(car);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Car>> getAllCars() {
+        List<Car> cars = carService.getAllCars();
+        return ResponseEntity.ok(cars);
     }
 }
