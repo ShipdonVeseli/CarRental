01e02c051b37ff4a25ef50a7ca4395434c2ebbbb
suekrueyavuz <93677903+suekrueyavuz@users.noreply.github.com>
2022-03-31 14:00:00 +0200

    get car in carService

--- src/main/java/com/carrental/service/CarService.java
+++ src/main/java/com/carrental/service/CarService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CarService {
@@ -22,5 +23,9 @@ public class CarService {
 
     public List<Car> getAllCars() {
         return carRepository.findAll();
+    }
+    
+    public Optional<Car> getCar(long id) {
+        return carRepository.findById(id);
     }
 }
