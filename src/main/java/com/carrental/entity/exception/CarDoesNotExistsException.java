dff56a64c8550262f00cdd052cd47389eeade2c4
suekrueyavuz <93677903+suekrueyavuz@users.noreply.github.com>
2022-04-06 13:25:48 +0200

    fixed syntax error

--- src/main/java/com/carrental/entity/exception/CarDoesNotExistsException.java
+++ src/main/java/com/carrental/entity/exception/CarDoesNotExistsException.java
@@ -4,6 +4,6 @@ import java.text.MessageFormat;
 
 public class CarDoesNotExistsException extends RuntimeException {
     public CarDoesNotExistsException(Long carId) {
-        super(MessageFormat.format("Car {0] does not exist.", carId));
+        super(MessageFormat.format("Car {0} does not exist.", carId));
     }
 }
