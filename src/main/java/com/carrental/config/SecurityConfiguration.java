127ddba483483f6e66b03f5ad693aeb1c1df713d
Sükrü Yavuz <yavuz.suekrue@gmail.com>
2022-04-07 02:40:20 +0200

    cors configuration

--- src/main/java/com/carrental/config/SecurityConfiguration.java
+++ src/main/java/com/carrental/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.carrental.config;
 
 //import com.carrental.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import com.carrental.filter.JwtFilter;
 import com.carrental.service.UserService;
@@ -21,6 +22,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -51,36 +53,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public CorsFilter corsFilter() {
-        String ALLOWEDHEADER = "Access-Control-Allow-Origin";
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:1234"));
-        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", ALLOWEDHEADER, "Content-Type",
-                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-                ALLOWEDHEADER, ALLOWEDHEADER, "Access-Control-Allow-Credentials"));
-        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-        return new org.springframework.web.filter.CorsFilter(urlBasedCorsConfigurationSource);
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Collections.singletonList("*"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().and()
-                .addFilterBefore(corsFilter(), SessionManagementFilter.class)
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/login", "/register", "/h2/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.headers().frameOptions().disable();
+                .anyRequest().authenticated();
     }
 }
